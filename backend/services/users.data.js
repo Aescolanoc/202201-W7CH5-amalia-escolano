9dcf54cd7d73f8587cfc48f9845b023375d33244
Mel Escolano <amalia.escolano@gmail.com>
2022-03-04 17:59:29 +0100

    Create index, db file and db.js tested

---
+++ backend/services/users.data.js
@@ -0,0 +1,20 @@
+export default {
+    users: [
+        {
+            name: 'Pepe',
+            passwd: '1234',
+            image: 'https://i.imgur.com/sEPM6Is.png',
+            about: '',
+            friends: [],
+            unfriends: [],
+        },
+        {
+            name: 'Ana',
+            passwd: '1234',
+            image: 'https://i.imgur.com/V2pRUjf.png',
+            about: '',
+            friends: [],
+            unfriends: [],
+        },
+    ],
+};
